ea1be5eeca3d5a5bfb7c76a0514174a011fb923a
P1 <Marco80512>
2022-03-30 09:28:14 -0700

    css

--- Finance.py
+++ Finance.py
@@ -1,1 +1,21 @@
+from flask import Flask, url_for, render_template, request, Markup
 
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+@app.route("/")
+def render_main():
+    return render_template('Main.html')    
+
+
+    
+@app.route("/Data")
+def render_response():
+   
+   
+  
+  
+    
+if __name__=="__main__":
+    app.run(debug=False)
+
+ 
