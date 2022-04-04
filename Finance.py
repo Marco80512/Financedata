e5ee5e0d583cfc191d1d9cbbdd0ce3f8ac543c07
P1 <Marco80512>
2022-04-04 09:48:18 -0700

    css

--- Finance.py
+++ Finance.py
@@ -6,9 +6,15 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('page1.html')    
     
-def Alabama_Data_Chart():
+def Alabama_Finance_Data():
     
-    
+    with open('finance.json') as finance_data:
+        Alabama = json.load(finance_data)
+    states=[]
+    for c in Alabama:
+       
+        if c ["State"] == "ALABAMA" and c ["Year"] == 1992:
+            
     
 
 
@@ -17,7 +23,7 @@ def Alabama_Data_Chart():
 def render_response():
    
    
-    if __name__=="__Main__":
+        if __name__=="__Main__":
         app.run(debug=False)
 
  
